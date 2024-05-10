65c1ff2a0958c08149056e9d00b2be68a2d03580
Rafael Rosa <rafael.o.rosa@gmail.com>
2024-05-09 23:01:37 -0300

    Asynchronous operations using Tasks (async, await)

--- WebApplication/Services/DepartmentService.cs
+++ WebApplication/Services/DepartmentService.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using WebApplication.Data;
 using WebApplication.Models;
 
@@ -14,9 +16,9 @@ namespace WebApplication.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
